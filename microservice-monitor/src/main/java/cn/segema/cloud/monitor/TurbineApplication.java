481c333cb5c8496302b33c87edd2bce060ed77a3
vangyong <vangyong@126.com>
2020-05-13 08:07:58 +0800

    modify turbine

--- microservice-monitor/src/main/java/cn/segema/cloud/monitor/TurbineApplication.java
+++ microservice-monitor/src/main/java/cn/segema/cloud/monitor/TurbineApplication.java
@@ -1,5 +1,14 @@
 package cn.segema.cloud.monitor;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+
 /**
  * 通过@EnableTurbine接口，激活对Turbine的支持。
  * @author eacdy
@@ -7,6 +16,7 @@ package cn.segema.cloud.monitor;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableDiscoveryClient
+@EnableHystrix
 @EnableHystrixDashboard
 @EnableTurbine
 public class TurbineApplication {
