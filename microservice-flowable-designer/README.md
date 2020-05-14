7a9c418685318072a3cd59d201e05d3417f98932
vangyong <vangyong@126.com>
2020-05-14 22:05:03 +0800

    modify flowablelle

--- microservice-flowable-designer/README.md
+++ microservice-flowable-designer/README.md
@@ -1,2 +1,5 @@
-
-参考：https://cloud.tencent.com/developer/article/1472549
+## 版本说明
+flowable-6.5.0
+## 参考：
+https://cloud.tencent.com/developer/article/1472549
+https://www.cnblogs.com/yangjiming/p/10944120.html
---
+++ microservice-flowable-designer/src/main/java/cn/segema/cloud/flowable/designer/config/SecurityConfig.java
@@ -0,0 +1,15 @@
+//package cn.segema.cloud.flowable.designer.config;
+//
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//public class SecurityConfig extends WebSecurityConfigurerAdapter {
+//
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {
+//		http.authorizeRequests()
+//				.anyRequest().permitAll().and().logout().permitAll();
+//	}
+//	
+//
+//}
