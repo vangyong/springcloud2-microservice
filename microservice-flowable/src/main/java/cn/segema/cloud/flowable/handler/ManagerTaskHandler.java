81663929a7e2e21b98821eebe12abacd1f6bf0b1
vangyong <vangyong@126.com>
2020-09-15 23:43:48 +0800

    add flowable demo

---
+++ microservice-flowable/src/main/java/cn/segema/cloud/flowable/handler/ManagerTaskHandler.java
@@ -0,0 +1,14 @@
+package cn.segema.cloud.flowable.handler;
+
+import org.flowable.engine.delegate.TaskListener;
+import org.flowable.task.service.delegate.DelegateTask;
+
+
+public class ManagerTaskHandler implements TaskListener {
+
+    @Override
+    public void notify(DelegateTask delegateTask) {
+        delegateTask.setAssignee("经理");
+    }
+
+}
